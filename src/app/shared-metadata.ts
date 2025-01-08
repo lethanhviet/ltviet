7f6890868c18269232067fd615bf8246934b92fc
lethanhviet <vietle@outlook.com>
2025-01-08 08:56:14 +0700

    Update shared-metadata.ts

--- src/app/shared-metadata.ts
+++ src/app/shared-metadata.ts
@@ -1,5 +1,5 @@
-export const sharedTitle = 'MonsterPi13'
-export const sharedDescription = 'Software Engineer, Dota enthusiast, based in Nanking, The China'
+export const sharedTitle = 'Viet T LE'
+export const sharedDescription = 'Engineering consultant, technology enthusiast, based in Hanoi, Vietnam & Bangkok, Thailand'
 export const sharedImage = {
   width: 1200,
   height: 630,
